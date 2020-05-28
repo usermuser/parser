4dc78eddcc926026ffbcf4a7636797f53455e983
usermuser <lwrdrstv@gmail.com>
2020-05-28 22:02:21 +0300

    dev: added BaseParser

--- habrclient.py
+++ habrclient.py
@@ -1,35 +1,114 @@
+import json
+import logging
 import time
 from urllib.parse import urlparse
 from typing import List
 
 from bs4 import BeautifulSoup
-import requests as req
+import requests
 from settings import (
     DEPTH,
     URL,
     DELAY,
     PAGES_TO_PARSE,
+    RETRY_COUNT,
+    RETRY_TIMEOUT,
+    RETRY,
+    REPEAT_TIMEOUT,
+    RETRY_CODES,
 )
 
 
-class HabrClient:
-    def __init__(self, url=URL, depth=DEPTH, quantity=PAGES_TO_PARSE):
+class BaseParser:
+    """Base Parser"""
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(level=logging.DEBUG)
+    REQUESTS_EXCEPTIONS = (requests.exceptions.ReadTimeout, requests.exceptions.ConnectTimeout,
+                           requests.exceptions.ConnectionError, requests.exceptions.HTTPError,
+                           requests.exceptions.RequestException)
+
+    def __init__(
+            self,
+            url=URL,
+            depth=DEPTH,
+            quantity=PAGES_TO_PARSE,
+            retry_count=RETRY_COUNT,
+            retry_timeout=RETRY_TIMEOUT,
+            retry=RETRY,
+            repeat_timeout=REPEAT_TIMEOUT,
+            retry_codes=RETRY_CODES,
+
+    ):
         self.url = url
         self.depth = depth
         self.pages = List
         self.delay = DELAY
         self.quantity = quantity
+        self.retry_count = retry_count
+        self.retry_timeout = retry_timeout
+        self.retry = retry
+        self.repeat_timeout = repeat_timeout
+        self.retry_codes = retry_codes
 
-    def parse(self):
+    @property
+    def _set_retries(self) -> int:
+        """Check settings for retry strategy"""
+
+        if self.retry:
+            return 3 if self.retry_count <= 0 else self.retry_count
+
+        if not self.retry <= 0:
+            return 1
+
+    def get(self, url, headers=None, payload=None):
+        """GET request with retries"""
+        tries = self._set_retries
+
+        while tries > 0:
+            try:
+                self.logger.info('Делаем запрос по адресу %s.' % url)
+                response = requests.get(url, headers=headers, json=payload)
+                if response.status_code == 200:
+                    return response.json()
+                elif response.status_code in self.retry_codes:
+                    self.logger.info('Будем делать повторный запрос так как получили %d.' % response.status_code, url)
+                    time.sleep(self.repeat_timeout)
+                else:
+                    return
+
+            except self.REQUESTS_EXCEPTIONS:
+                self.logger.exception('Запрос по адресу %s не удался.' % url)
+
+            except json.decoder.JSONDecodeError:
+                self.logger.exception('Запросе по адресу %s не удался: в ответe не JSON: %s' % url, response)
+
+            except Exception:
+                self.logger.exception('Внутренняя ошибка!')
+
+            time.sleep(self.retry_timeout)
+            self.logger.info('Будем делать повторный запрос так как получили %d.' % response.status_code, url)
+            tries -= 1
+        self.logger.error('Запрос по адресу %s не удался' % url)
+        return
+
+
+class HabrClient(BaseParser):
+
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
         while self.depth > 0 or self.quantity > 0:
             time.sleep(self.delay)
+
             self.pages.append()
             # do some work
             self.quantity -= 1
             self.depth -= 1
         return
 
-
+    def parse(self):
+        response = self.get(self.url)
 
     def _remove_slash(self):
         pass
@@ -43,4 +122,3 @@ class HabrClient:
 
 if __name__ == '__main__':
     client = HabrClient()
-    print(client)
