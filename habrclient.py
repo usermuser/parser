dcbf581189b397da29d5ccdf25a6fd9c87a1109a
usermuser <lwrdrstv@gmail.com>
2020-05-29 01:26:11 +0300

    dev: adder run, save_pages, write_to_file methods

--- habrclient.py
+++ habrclient.py
@@ -16,6 +16,9 @@ from settings import (
     RETRY,
     REPEAT_TIMEOUT,
     RETRY_CODES,
+    PAGES_FOLDER,
+    BASE_DIR,
+
 )
 
 
@@ -30,8 +33,6 @@ class BaseParser:
     def __init__(
             self,
             url=URL,
-            depth=DEPTH,
-            quantity=PAGES_TO_PARSE,
             retry_count=RETRY_COUNT,
             retry_timeout=RETRY_TIMEOUT,
             retry=RETRY,
@@ -40,10 +41,6 @@ class BaseParser:
 
     ):
         self.url = url
-        self.depth = depth
-        self.pages = List
-        self.delay = DELAY
-        self.quantity = quantity
         self.retry_count = retry_count
         self.retry_timeout = retry_timeout
         self.retry = retry
@@ -67,9 +64,9 @@ class BaseParser:
         while tries > 0:
             try:
                 self.logger.info('Делаем запрос по адресу %s.' % url)
-                response = requests.get(url, headers=headers, json=payload)
+                response = requests.get(url)
                 if response.status_code == 200:
-                    return response.json()
+                    return response
                 elif response.status_code in self.retry_codes:
                     self.logger.info('Будем делать повторный запрос так как получили %d.' % response.status_code, url)
                     time.sleep(self.repeat_timeout)
@@ -94,27 +91,57 @@ class BaseParser:
 
 class HabrClient(BaseParser):
 
-    def __init__(self):
+    def __init__(self, depth=DEPTH, pages_to_parse=PAGES_TO_PARSE):
         super().__init__()
+        self.depth = depth
+        self.pages = List
+        self.delay = DELAY
+        self.pages_to_parse = pages_to_parse
+        self.urls = [[self.url]]
+
 
     def run(self):
-        while self.depth > 0 or self.quantity > 0:
-            time.sleep(self.delay)
-
-            self.pages.append()
-            # do some work
-            self.quantity -= 1
+        try:
+            urls_to_save = self.urls.pop()
             self.depth -= 1
+            self.save_pages(urls_to_save)
+        except IndexError:
+            print(f'Все страницы сохранены')
+            # Open files
+            # Count words
         return
 
-    def parse(self):
-        response = self.get(self.url)
+    def save_pages(self, _urls):
+        for url in _urls:
+            page_content = self.get_page_content(url)
+            pagename = urlparse(url).netloc
+            self.write_to_file(pagename, page_content)
+            self.pages_to_parse -= 1
+            if self.pages_to_parse <= 0:
+                return
+        return
 
-    def _remove_slash(self):
-        pass
+    @staticmethod
+    def write_to_file(_filename, _page_content):
+        __file_path = f'{PAGES_FOLDER}/{_filename}.html'
+        with open(__file_path, mode='w+') as file:
+            try:
+                file.write(_page_content.text)
+            except Exception as e:
+                print(f'Exception occured: {e}')
+        return
 
-    def from_same_domain(self):
-        pass
+    def get_page_content(self, _url):
+        return self.get(_url)
+
+    # def extract_urls(self, response):
+    #     result = []
+    #
+    #     for raw_link in soup.find_all('a'):
+    #         link = raw_link.get('href')
+    #         if link.startswith(self.url) and link != self.url:
+    #             result.append(link)
+    #     return result
 
     def __repr__(self):
         return f'Client for {self.url}'
@@ -122,3 +149,4 @@ class HabrClient(BaseParser):
 
 if __name__ == '__main__':
     client = HabrClient()
+    client.run()
--- settings.py
+++ settings.py
@@ -1,7 +1,12 @@
-DEPTH = 1
+import os
+
+DEPTH = 2
 DELAY = 1.0
 PAGES_TO_PARSE = 5
 URL = 'https://habr.com/ru/'
+BASE_DIR = os.getcwd()
+PAGES_FOLDER = os.path.join(BASE_DIR, 'saved_pages')
+print(f'pages folder: {PAGES_FOLDER}')
 
 # RETRY STRATEGY
 RETRY_COUNT = 4
